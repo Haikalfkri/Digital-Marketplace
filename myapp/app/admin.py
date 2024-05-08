3b0464c1305d2095a6d1a02257243dbd1e7e4f75
Haikal Fikri <haikaljr1245@gmail.com>
2024-05-08 16:06:23 +0700

    OrderDetail model

--- myapp/app/admin.py
+++ myapp/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import Product
+from app.models import Product, OrderDetail
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(OrderDetail)
