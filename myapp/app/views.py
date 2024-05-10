1bd896418d10de45cacf81f573f14131d634213d
Haikal Fikri <haikaljr1245@gmail.com>
2024-05-10 14:54:50 +0700

    fix invalid email adress and create stripe payment intent

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -19,46 +19,54 @@ def detail(request, id):
 
 
 @csrf_exempt
-def create_checkout_session(request, id):
+def create_checkout_session(request,id):
     request_data = json.loads(request.body)
     product = Product.objects.get(id=id)
     stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripePayemntIntent = stripe.PaymentIntent.create(
+        amount = int(product.price * 100),
+        currency = 'usd',
+        metadata={'integration_check': 'accept_a_payment'},
+    )
     checkout_session = stripe.checkout.Session.create(
         customer_email = request_data['email'],
         payment_method_types = ['card'],
-        line_items = [
+        line_items=[
             {
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': product.name,
+                'price_data':{
+                    'currency':'usd',
+                    'product_data':{
+                        'name':product.name,
                     },
-                    'unit_amount': int(product.price * 100)
+                    'unit_amount':int(product.price * 100)
                 },
-                'quantity': 1,
+                'quantity':1,
             }
         ],
         mode='payment',
-        success_url = request.build_absolute_uri(reverse('success')) + 
+        success_url = request.build_absolute_uri(reverse('success')) +
         "?session_id={CHECKOUT_SESSION_ID}",
         cancel_url = request.build_absolute_uri(reverse('failed')),
+        
     )
     
-    order = OrderDetail()
-    order.customer_email = request_data['email']
-    order.product = product
-    order.stripe_payment_intent = checkout_session['payment_intent']
-    order.amount = int(product.price)
+    order = OrderDetail.objects.create(
+        customer_email = request_data['email'],
+        product = product,
+        stripe_payment_intent = stripePayemntIntent['id'],
+        amount =  int(product.price),
+    )
+    
     order.save()
     
-    return JsonResponse({'sessionId': checkout_session.id})
+    return JsonResponse({'sessionId':checkout_session.id})
 
 
 def payment_success_view(request):
     session_id = request.GET.get('session_id')
     if session_id is None:
         return HttpResponseNotFound()
-    stripe.api_key = settings.STRIPE_API_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     session = stripe.checkout.Session.retrieve(session_id)
     order = get_object_or_404(OrderDetail, stripe_payment_intent = session.payment_intent)
     order.has_paid = True
